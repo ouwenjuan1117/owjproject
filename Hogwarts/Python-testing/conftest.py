856c2a494e07da4c5ceea4148dadc777393c6c9d
“ouwenjuan1117 <1119770743@qq.com>
2020-08-10 18:58:38 +0800

    pytest加减乘除第二次作业

---
+++ Hogwarts/Python-testing/conftest.py
@@ -0,0 +1,139 @@
+import os
+
+import pytest
+import yaml
+import sys
+
+# 获取命令行的当前路径
+sys.path.append('../..')
+from Hogwarts.pythoncode.calc import Calclator
+
+
+# def get_root_dir():
+#     return os.path.dirname(os.path.dirname(
+#         os.path.dirname(__file__)))
+#
+# sys.path.append(get_root_dir())
+
+def pytest_collection_modifyitems(items):
+    '''
+    测试用例收集完成时，将收集到的item的name和nodeid的中文显示在控制台上
+    :param items:
+    :return:
+    '''
+    for item in items:
+        item.name = item.name.encode("utf-8").decode("unicode_escape")
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
+
+
+@pytest.fixture(scope='module')
+def connectDB():
+    print('连接数据库操作')
+    yield
+    print('断开数据库连接')
+
+
+@pytest.fixture(scope='class')
+def get_calc():
+    print('获取计算器的实例')
+    calc = Calclator()
+    return calc
+
+
+yamlfilepath = os.path.dirname(__file__) + "/datas/calc.yml"
+with open(yamlfilepath, encoding='UTF-8') as f:
+    # 获取加法的参数
+    datas_aa = yaml.safe_load(f)
+    add_datas = datas_aa['add']['add_datas']
+    # 获取加法测试用例的标题
+    add_ids = datas_aa['add']['add_ids']
+
+    # 获取除法的参数
+    div_datas = datas_aa['div']['div_datas']
+    # 获取除法测试用例的标题
+    div_ids = datas_aa['div']['div_ids']
+
+    # 获取乘法成功用例的参数的参数
+    mul_datas_success = datas_aa['mul']['mul_datas_success']
+    # 获取乘法成功测试测试用例的标题
+    mul_ids_success = datas_aa['mul']['mul_ids_success']
+
+    # 获取乘法失败用例的参数的参数
+    mul_datas_fail = datas_aa['mul']['mul_datas_fail']
+    # 获取乘法失败测试测试用例的标题
+    mul_ids_fail = datas_aa['mul']['mul_ids_fail']
+
+    # 获取减法成功用例的参数的参数
+    sub_datas_success = datas_aa['sub']['sub_datas_success']
+    print(sub_datas_success)
+    # 获取减法成功测试测试用例的标题
+    sub_ids_success = datas_aa['sub']['sub_ids_success']
+    print(sub_ids_success)
+
+    # 获取减法失败用例的参数的参数
+    sub_datas_fail = datas_aa['sub']['sub_datas_fail']
+    print(sub_datas_fail)
+    # 获取减法失败测试测试用例的标题
+    sub_ids_fail = datas_aa['sub']['sub_ids_fail']
+    print(sub_ids_fail)
+
+
+# 想要在方法前打印开始计算，结束计算，可以用yield
+@pytest.fixture(params=add_datas, ids=add_ids)
+# 获得加法的数据
+def get_datas_add(request):
+    print('开始计算')
+    datas = request.param
+    print(f'加法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
+
+
+@pytest.fixture(params=div_datas, ids=div_ids)
+# 获得除法的数据
+def get_datas_div(request):
+    print('开始计算')
+    datas = request.param
+    print(f'减法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
+
+
+@pytest.fixture(params=mul_datas_success, ids=mul_ids_success)
+# 获得乘法成功用例的数据
+def get_datas_mul_success(request):
+    print('开始计算')
+    datas = request.param
+    print(f'减法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
+
+
+@pytest.fixture(params=mul_datas_fail, ids=mul_ids_fail)
+# 获得乘法失败用例的数据
+def get_datas_mul_fail(request):
+    print('开始计算')
+    datas = request.param
+    print(f'减法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
+
+
+@pytest.fixture(params=sub_datas_success, ids=sub_ids_success)
+# 获得乘法成功用例的数据
+def get_datas_sub_success(request):
+    print('开始计算')
+    datas = request.param
+    print(f'减法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
+
+
+@pytest.fixture(params=sub_datas_fail, ids=sub_ids_fail)
+# 获得乘法失败用例的数据
+def get_datas_sub_fail(request):
+    print('开始计算')
+    datas = request.param
+    print(f'减法的测试数据是：{datas}')
+    yield datas
+    print('结束计算')
---
+++ Hogwarts/Python-testing/test_calc_new.py
@@ -0,0 +1,107 @@
+import os
+import sys
+# 获取命令行的当前路径
+import yaml
+
+sys.path.append('../..')
+yamlfilepath = os.path.dirname(__file__) + "/datas/calc.yml"
+with open(yamlfilepath, encoding='UTF-8') as f:
+    # 获取加法的参数
+    datas_aa = yaml.safe_load(f)
+
+    # 获取乘法成功用例的参数的参数
+    mul_datas_success = datas_aa['mul']['mul_datas_success']
+    print(mul_datas_success)
+    # 获取乘法成功测试测试用例的标题
+    mul_ids_success = datas_aa['mul']['mul_ids_success']
+    print(mul_ids_success)
+
+    # 获取乘法失败用例的参数的参数
+    mul_datas_fail = datas_aa['mul']['mul_datas_fail']
+    print(mul_datas_fail)
+    # 获取乘法失败测试测试用例的标题
+    mul_ids_fail = datas_aa['mul']['mul_datas_fail']
+    print(mul_ids_fail)
+
+
+class TestCalc():
+
+    def test_add(self, get_calc, get_datas_add):
+        try:
+            # 调用它的加法的方法
+            result = get_calc.add(get_datas_add[0], get_datas_add[1])
+            if isinstance(result, float):
+                result = round(result, 2)
+        except:
+            if isinstance(get_datas_add[0], str) or isinstance(get_datas_add[1], str):
+                raise TypeError('计算器不支持字符串')
+            else:
+                print(get_datas_add[0])
+                print(get_datas_add[1])
+                print('请输入正确的数据类型')
+        else:
+            assert get_datas_add[2] == result
+
+    def test_div(self, get_calc, get_datas_div):
+        if get_datas_div[1] == 0:
+            print('除数不能为0')
+        try:
+            # 调用它的除法的方法
+            result = get_calc.div(get_datas_div[0], get_datas_div[1])
+            if isinstance(result, float):
+                result = round(result, 2)
+        except:
+            if isinstance(get_datas_div[0], str) or isinstance(get_datas_div[1], str):
+                raise TypeError('计算器不支持字符串')
+            else:
+                print(get_datas_div[0])
+                print(get_datas_div[1])
+                print('请输入正确的数据类型')
+        else:
+            assert get_datas_div[2] == result
+
+    # 乘法成功的测试用例
+    def test_mul_success(self, get_calc, get_datas_mul_success):
+        # 调用它的减法的方法
+        result = get_calc.mul(get_datas_mul_success[0], get_datas_mul_success[1])
+        if isinstance(result, float):
+            result = round(result, 2)
+        else:
+            assert get_datas_mul_success[2] == result
+
+    # 乘法失败的测试用例
+    def test_mul_fail(self, get_calc, get_datas_mul_fail):
+        try:
+            result = get_calc.mul(get_datas_mul_fail[0], get_datas_mul_fail[1])
+        except:
+            if isinstance(get_datas_mul_fail[0], str) or isinstance(get_datas_mul_fail[1], str):
+                raise TypeError('计算器不支持字符串')
+            else:
+                print(get_datas_mul_fail[0])
+                print(get_datas_mul_fail[1])
+                print('请输入正确的数据类型')
+        else:
+            assert get_datas_mul_fail[2] == result
+
+    # 减法成功的测试用例
+    def test_sub_success(self, get_calc, get_datas_sub_success):
+        # 调用它的减法的方法
+        result = get_calc.sub(get_datas_sub_success[0], get_datas_sub_success[1])
+        if isinstance(result, float):
+            result = round(result, 2)
+        else:
+            assert get_datas_sub_success[2] == result
+
+    # 减法失败的测试用例
+    def test_sub_fail(self, get_calc, get_datas_sub_fail):
+        try:
+            result = get_calc.sub(get_datas_sub_fail[0], get_datas_sub_fail[1])
+        except:
+            if isinstance(get_datas_sub_fail[0], str) or isinstance(get_datas_sub_fail[1], str):
+                raise TypeError('计算器不支持字符串')
+            else:
+                print(get_datas_sub_fail[0])
+                print(get_datas_sub_fail[1])
+                print('请输入正确的数据类型')
+        else:
+            assert get_datas_sub_fail[2] == result
