c0eb6c81ef28205e4fe81b34d7dfd6130c516e9d
“ouwenjuan1117 <1119770743@qq.com>
2020-08-11 13:56:37 +0800

    pytest第三次作业

---
+++ Hogwarts/Python-testing/test_order.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+import pytest
+
+
+@pytest.mark.run(order=3)
+def test_mul():
+    sleep(0.5)
+    assert True
+
+
+@pytest.mark.run(order=2)
+def test_sub():
+    sleep(0.5)
+    assert True
+
+
+@pytest.mark.last
+def test_div():
+    sleep(0.5)
+    assert True
+
+
+@pytest.mark.run(order=1)
+def test_add():
+    sleep(0.5)
+    assert True
