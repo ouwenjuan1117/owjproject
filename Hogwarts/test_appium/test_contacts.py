b1be8a9c5eb4533d915bbdf2fe2911349e19cd45
“ouwenjuan1117 <1119770743@qq.com>
2020-09-07 16:45:40 +0800

    联系人参数化

--- Hogwarts/test_appium/test_contacts.py
+++ Hogwarts/test_appium/test_contacts.py
@@ -1,8 +1,19 @@
 from time import sleep
+
+import pytest
+import yaml
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.support.wait import WebDriverWait
 
+
+# 创建一个方法，用来读取yml文件
+def get_datas():
+    with open("./datas/contacts.yml", encoding='utf-8') as f:
+        contact_datas = yaml.safe_load(f)
+        addcontact = contact_datas['add']
+        delcontact = contact_datas['del']
+    return [addcontact, delcontact]
 
 class TestContacts:
     def setup(self):
@@ -27,14 +38,20 @@ class TestContacts:
         # self.driver.back()
         self.driver.quit()
 
-    def test_addcontact(self):
+    # @pytest.mark.parametrize("name","gender","phonenum",[
+    #     ["hogwarts002","女","13325288855"],
+    #     ["hogwarts003", "女", "13325288856"]
+    # ])
+
+    @pytest.mark.parametrize("name", "gender", "phonenum", get_datas()[0])
+    def test_addcontact(self, name, gender, phonenum):
         """
         添加联系人
         :return:
         """
-        name = 'hogwarts002'
-        gender = '女'
-        phonenum = '13325288855'
+        # name = 'hogwarts002'
+        # gender = '女'
+        # phonenum = '13325288855'
         # 定位到通讯录，并做点击操作
         self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
         # 点击 添加成员，当成员很多时不会显示在第一页，需要滑动到最后
@@ -67,44 +84,63 @@ class TestContacts:
         # 打印下页面布局，看布局下有没有 toast
         # print(self.driver.page_source)
         mytoast = self.driver.find_element(MobileBy.XPATH, "//*[@text='添加成功']").text
+
         assert '添加成功' == mytoast
 
-    def test_delete_contact(self):
+    @pytest.mark.parametrize("membername", get_datas()[1])
+    def test_delete_contact(self, membername):
         """
         删除成员
         :return:
         """
-        membername = 'hogwarts002'
+        # membername = 'hogwarts002'
         # 定位到通讯录，并做点击操作
         self.driver.find_element(MobileBy.XPATH, "//*[@text='通讯录']").click()
         # 定位到 搜索
         self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/guu").click()
         # 定位到搜索输入框
         self.driver.find_element(MobileBy.XPATH, "//*[@text='搜索']").send_keys(membername)
-        # 确定成员是否存在
+        sleep(2)
+        # #判断成员是否存在，存在的话 membername文本应该为2，不存在的话那就只有一个membername(老师写的方式)
+        # eles = self.driver.find_elements(MobileBy.XPATH, f"//*[@text='{membername}']")
+        # beforenum = len(eles)
+        # if beforenum < 2:
+        #     print("没有可删除人员")
+        #     # return 不进行后面的操作
+        #     return
+        # # 否则的话，继续往下走，找到第二个进行点击操作
+        # eles[1].click()
+
+        # # 确定成员是否存在（自己写的方式）
         try:
             elename = self.driver.find_element(MobileBy.XPATH,
                                                "//*[@text='hogwarts002' and @class='android.widget.TextView']").text
             assert membername == elename
-            self.driver.find_element(MobileBy.XPATH,
-                                     "//*[@text='hogwarts002' and @class='android.widget.TextView']").click()
-            # 进入个人信息页面
-            self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/guk").click()
-            # 点击 编辑成员
-            self.driver.find_element(MobileBy.XPATH, "//*[@text='编辑成员']").click()
-            # 点击删除成员
-            # self.driver.find_element(MobileBy.XPATH, "//*[@text='删除成员']").click()
-            self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
-                                     'new UiScrollable(new UiSelector()'
-                                     '.scrollable(true).instance(0))'
-                                     '.scrollIntoView(new UiSelector()'
-                                     '.text("删除成员").instance(0));').click()
-            # 点击 确定 删除
-            self.driver.find_element(MobileBy.XPATH, "//*[@text='确定']").click()
-            # 判断是否删除成功
-            eleassert = self.driver.find_element(MobileBy.XPATH, f"//*[@text='网络查找手机/邮箱：{membername}']").text
-            assert '网络查找手机/邮箱：{membername}' == eleassert
         except:
             print("成员不存在")
-        finally:
-            self.driver.back()
+        self.driver.find_element(MobileBy.XPATH,
+                                 "//*[@text='hogwarts002' and @class='android.widget.TextView']").click()
+
+        # 进入个人信息页面
+        self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/guk").click()
+        # 点击 编辑成员
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='编辑成员']").click()
+        # 点击删除成员
+        # self.driver.find_element(MobileBy.XPATH, "//*[@text='删除成员']").click()
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR,
+                                 'new UiScrollable(new UiSelector()'
+                                 '.scrollable(true).instance(0))'
+                                 '.scrollIntoView(new UiSelector()'
+                                 '.text("删除成员").instance(0));').click()
+        # 点击 确定 删除
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='确定']").click()
+        sleep(2)
+        # 判断是否删除成功(自己写的方式)
+        eles1 = self.driver.find_elements(MobileBy.XPATH, f"//*[@text='{membername}']")
+        afternum = len(eles1)
+        assert afternum == 1
+
+        # # 老师写的方法，判断membername个数是不是比之前少了一个
+        # eles1 = self.driver.find_elements(MobileBy.XPATH, f"//*[@text='{membername}']")
+        # afternum = len(eles1)
+        # assert afternum == beforenum - 1
